99c30e66b7ed0e07bdc24ff4dff6d057c08ac256
dimitriostsakiridis <tsakijim2@outlook.com>
2025-07-06 04:01:26 +0300

    feat/add: added a repository method to check for match instances between two teams on a specific date.
    -

--- src/main/java/com/auth/match_management/repositories/MatchRepository.java
+++ src/main/java/com/auth/match_management/repositories/MatchRepository.java
@@ -3,5 +3,11 @@ package com.auth.match_management.repositories;
 import com.auth.match_management.entities.MatchEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
 public interface MatchRepository extends JpaRepository<MatchEntity, Long> {
+    //Check for match instances between two teams on a specific date
+    boolean existsByTeamHomeNameAndTeamAwayNameAndDate(String teamHomeName, String teamAwayName, Date date);
 }
